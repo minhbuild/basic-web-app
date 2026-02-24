6f75c9b37360bc5bb5ad008a8ca0ae4d7e2c5730
minhbuild <minhd@andrew.cmu.edu>
2026-02-24 10:38:51 -0500

    minus q

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -166,5 +166,28 @@ export default function QueryProcessor(query: string): string {
     return anagrams.length > 0 ? anagrams.join(', ') : "";
   }
 
+  // Handle "What is the scrabble score of [word]?" generalized
+  const scrabbleMatch = query.match(/what is the scrabble score of\s+(\w+)/i);
+  if (scrabbleMatch) {
+    const word = scrabbleMatch[1].toLowerCase();
+    
+    const scrabbleValues: { [key: string]: number } = {
+      a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1,
+      d: 2, g: 2,
+      b: 3, c: 3, m: 3, p: 3,
+      f: 4, h: 4, v: 4, w: 4, y: 4,
+      k: 5,
+      j: 8, x: 8,
+      q: 10, z: 10
+    };
+    
+    let score = 0;
+    for (const letter of word) {
+      score += scrabbleValues[letter] || 0;
+    }
+    
+    return score.toString();
+  }
+
   return "";
 }
