deba04535d2777cc156a2f18db3b0a3019b6d3ac
minhbuild <minhd@andrew.cmu.edu>
2026-02-24 09:52:38 -0500

    added name q

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -23,5 +23,9 @@ export default function QueryProcessor(query: string): string {
     return "minhd";
   }
 
+  if (query.toLowerCase().includes("name")) {
+    return "Minh";
+  }
+
   return "";
 }
